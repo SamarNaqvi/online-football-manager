eb55bfbfc3f90c48eb6cab49d6126506e36fbe91
Samar Abbas <samarabbas@ITs-MacBook-Pro.local>
2025-07-27 16:22:34 +0500

    seeding logic added

--- server/src/app/modules/prisma/prisma.service.ts
+++ server/src/app/modules/prisma/prisma.service.ts
@@ -1,10 +1,27 @@
-import { Injectable, type OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
+import { Injectable, type OnModuleInit } from '@nestjs/common';
+import { Player, PrismaClient } from '@prisma/client';
+import path from 'path';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   /* istanbul ignore next */
   async onModuleInit(): Promise<void> {
     await this.$connect();
+    await this.runSeed();
+  }
+
+  async runSeed() {
+    const playerCount = await this.player.count();
+    if (playerCount == 0) {
+      console.log(
+        '\n----------------Seeding DataBase With Players--------------------',
+      );
+      const { players } = await import(`../../../../prisma/seed/players`);
+      const createPromises = players.map((playerData) =>
+        this.player.create({ data: playerData }),
+      );
+      await Promise.all(createPromises);
+      console.log(`\n--------------Seeded ${players.length} players successfully.-----------------`);
+    }
   }
 }
